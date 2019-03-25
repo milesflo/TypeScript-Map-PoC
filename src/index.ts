8b3f43f66d797396745c19a783115606de1c3a81
Miles <miles.b.flo@gmail.com>
2019-03-25 12:19:37 -0700

    Adding comments to PoC

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import items from "./utils/items"
 
+// Items 
+console.log(items)
+
+// Item is an array, despite it being of type Map... interesting.
+console.log(Array.isArray(items) ? "Item is Array" : "Item is not Array")
+
+// `has` is not defined
 try {
   items.has('hat')
 } catch (e) {
   console.log(e.message)
-  console.log("whyyyyy")
 }
 
-
+// `get` is not defined
+try {
+  items.get('hat')
+} catch (e) {
+  console.log(e.message)
+}
